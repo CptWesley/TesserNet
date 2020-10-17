79f47939d61662799bf14dcb4eda7b7060873d79
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2020-10-17 18:46:10 +0200

    Add example project for System.Drawing

---
+++ src/TesserNet.Example.System.Drawing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using TesserNet.System.Drawing;
+
+namespace TesserNet.Example.System.Drawing
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("TesserNet.Example.System.Drawing.img.png");
+            Image image = Image.FromStream(stream);
+            Tesseract tesseract = new Tesseract();
+
+            Console.WriteLine(tesseract.Read(image));
+
+            stream.Dispose();
+            image.Dispose();
+            tesseract.Dispose();
+        }
+    }
+}
