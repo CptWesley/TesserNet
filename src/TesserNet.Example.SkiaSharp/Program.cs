cacb0ae4e70d057f5c602f1913e9b238272e2737
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-11-09 22:29:49 +0100

    Add support library for SkiaSharp

---
+++ src/TesserNet.Example.SkiaSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using SkiaSharp;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace TesserNet.Example.ImageSharp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("TesserNet.Example.SkiaSharp.img.png");
+            SKBitmap image = SKBitmap.Decode(stream);
+            Tesseract tesseract = new Tesseract();
+
+            Console.WriteLine(tesseract.Read(image).Trim());
+
+            stream.Dispose();
+            image.Dispose();
+            tesseract.Dispose();
+        }
+    }
+}
---
+++ src/TesserNet.SkiaSharp/SkiaSharpTesseractExtensions.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace TesserNet
+{
+    /// <summary>
+    /// Provides extension methods for the <see cref="Tesseract"/> class.
+    /// </summary>
+    public static class SkiaSharpTesseractExtensions
+    {
+        /// <summary>
+        /// Performs OCR on the given image.
+        /// </summary>
+        /// <param name="tesseract">The tesseract instance.</param>
+        /// <param name="image">The image.</param>
+        /// <returns>The found text as a UTF8 string.</returns>
+        public static string Read(this ITesseract tesseract, SKBitmap image)
+            => tesseract.Read(image, new SKRect(-1, -1, -1, -1));
+
+        /// <summary>
+        /// Performs OCR on a rectangle inside the given image.
+        /// </summary>
+        /// <param name="tesseract">The tesseract instance.</param>
+        /// <param name="image">The image.</param>
+        /// <param name="rectangle">The rectangle to perform OCR in.</param>
+        /// <returns>The found text as a UTF8 string.</returns>
+        public static string Read(this ITesseract tesseract, SKBitmap image, SKRect rectangle)
+        {
+            if (tesseract is null)
+            {
+                throw new ArgumentNullException(nameof(tesseract));
+            }
+
+            if (image is null)
+            {
+                throw new ArgumentNullException(nameof(image));
+            }
+
+            byte[] data = BitmapToBytes(image);
+            return tesseract.Read(data, image.Width, image.Height, 4, (int)rectangle.Left, (int)rectangle.Top, (int)rectangle.Width, (int)rectangle.Height);
+        }
+
+        /// <summary>
+        /// Performs OCR on the given image.
+        /// </summary>
+        /// <param name="tesseract">The tesseract instance.</param>
+        /// <param name="image">The image.</param>
+        /// <returns>The found text as a UTF8 string.</returns>
+        public static Task<string> ReadAsync(this ITesseract tesseract, SKBitmap image)
+            => tesseract.ReadAsync(image, new SKRect(-1, -1, -1, -1));
+
+        /// <summary>
+        /// Performs OCR on a rectangle inside the given image.
+        /// </summary>
+        /// <param name="tesseract">The tesseract instance.</param>
+        /// <param name="image">The image.</param>
+        /// <param name="rectangle">The rectangle to perform OCR in.</param>
+        /// <returns>The found text as a UTF8 string.</returns>
+        public static Task<string> ReadAsync(this ITesseract tesseract, SKBitmap image, SKRect rectangle)
+        {
+            if (tesseract is null)
+            {
+                throw new ArgumentNullException(nameof(tesseract));
+            }
+
+            if (image is null)
+            {
+                throw new ArgumentNullException(nameof(image));
+            }
+
+            byte[] data = BitmapToBytes(image);
+            return tesseract.ReadAsync(data, image.Width, image.Height, 4, (int)rectangle.Left, (int)rectangle.Top, (int)rectangle.Width, (int)rectangle.Height);
+        }
+
+        /// <summary>
+        /// Performs OCR on the given image.
+        /// </summary>
+        /// <param name="tesseract">The tesseract instance.</param>
+        /// <param name="image">The image.</param>
+        /// <returns>The found text as a UTF8 string.</returns>
+        public static string Read(this ITesseract tesseract, SKImage image)
+            => tesseract.Read(SKBitmap.FromImage(image));
+
+        /// <summary>
+        /// Performs OCR on a rectangle inside the given image.
+        /// </summary>
+        /// <param name="tesseract">The tesseract instance.</param>
+        /// <param name="image">The image.</param>
+        /// <param name="rectangle">The rectangle to perform OCR in.</param>
+        /// <returns>The found text as a UTF8 string.</returns>
+        public static string Read(this ITesseract tesseract, SKImage image, SKRect rectangle)
+            => tesseract.Read(SKBitmap.FromImage(image), rectangle);
+
+        /// <summary>
+        /// Performs OCR on the given image.
+        /// </summary>
+        /// <param name="tesseract">The tesseract instance.</param>
+        /// <param name="image">The image.</param>
+        /// <returns>The found text as a UTF8 string.</returns>
+        public static Task<string> ReadAsync(this ITesseract tesseract, SKImage image)
+            => tesseract.ReadAsync(SKBitmap.FromImage(image));
+
+        /// <summary>
+        /// Performs OCR on a rectangle inside the given image.
+        /// </summary>
+        /// <param name="tesseract">The tesseract instance.</param>
+        /// <param name="image">The image.</param>
+        /// <param name="rectangle">The rectangle to perform OCR in.</param>
+        /// <returns>The found text as a UTF8 string.</returns>
+        public static Task<string> ReadAsync(this ITesseract tesseract, SKImage image, SKRect rectangle)
+            => tesseract.ReadAsync(SKBitmap.FromImage(image), rectangle);
+
+        private static byte[] BitmapToBytes(SKBitmap bmp)
+            => bmp.GetPixelSpan().ToArray();
+    }
+}
