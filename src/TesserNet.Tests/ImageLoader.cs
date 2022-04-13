5c8ef67597c5c779656194591d6a415f0cf68910
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2022-04-13 18:34:08 +0200

    Add simple tests

---
+++ src/TesserNet.Tests/ImageLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace TesserNet.Tests
+{
+    /// <summary>
+    /// Used to load images.
+    /// </summary>
+    internal static class ImageLoader
+    {
+        /// <summary>
+        /// Loads an image as a stream.
+        /// </summary>
+        /// <param name="fileName">The filename.</param>
+        /// <returns>The stream.</returns>
+        public static Stream LoadStream(string fileName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            return asm.GetManifestResourceStream($"TesserNet.Tests.Resources.{fileName}");
+        }
+
+        /// <summary>
+        /// Loads an image as a byte array.
+        /// </summary>
+        /// <param name="fileName">The filename.</param>
+        /// <returns>The stream.</returns>
+        public static byte[] LoadByteArray(string fileName)
+        {
+            using MemoryStream ms = new MemoryStream();
+            using Stream s = LoadStream(fileName);
+            s.CopyTo(ms);
+            return ms.ToArray();
+        }
+    }
+}
